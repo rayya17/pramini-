79c61841f649a768394e394012ae5623d4b48bc6
putra03pratama <putra09prtama@gmail.com>
2023-10-16 13:25:25 +0700

    admin dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
  use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'admin',
-            'email' => 'admin.gmail.com',
+            'email' => 'admin@gmail.com',
+            'password' =>Hash::make('12345678'),
             'role' => 'admin',
         ]);
     }
