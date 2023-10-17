617c26bed30f5d383f050541786e13615d65b9ed
rayya17 <rayyakhnsa@gmail.com>
2023-10-17 08:33:13 +0700

    p

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-
             <div class="col-md-8">
-
                 <div class="card">
                     <ul class="navbar-nav me-auto">
 
