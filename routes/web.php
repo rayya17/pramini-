6af900877cc26308fb5fb678e62d1891e7d19e74
putra03pratama <putra09prtama@gmail.com>
2023-10-25 09:20:46 +0700

    pengguna admin

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,20 @@ use App\Http\Controllers\dashuserController;
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 
+// Route Admin
 Route::middleware('cekAdmin')->group(function () {
-    Route::get('dashboardAdmin', [adminController::class, 'dashboardAdmin'])->name('dashboardAdmin');
+    Route::get('dashboard', [adminController::class, 'dashboard'])->name('dashboard');
     Route::get('transaksiAdmin', [adminController::class, 'transaksiAdmin'])->name('transaksiAdmin');
     Route::resource('pembayaranadmin', App\Http\Controllers\adminController::class);
     Route::get('aedit/{id}/edit', [adminController::class, 'aedit'])->name('aedit');
     Route::put('aupdate/{id}', [adminController::class, 'aupdate'])->name('aupdate');
     Route::delete('adestroy/{adminmp}', [adminController::class, 'adestroy'])->name('adestroy');
+    Route::get('dashboard', [adminController::class, 'dashboard'])->name('dashboard');
+    Route::get('kepengguna', [adminController::class, 'kepengguna'])->name('kepengguna');
     Route::resource('kamar', KamarController::class);
 });
 
+// Route User
 Route::middleware('UserMiddleware')->group(function () {
     Route::get('dashboardUser', [dashuserController::class, 'dashboardUser'])->name('dashboardUser');
     Route::get('pesanan', [dashuserController::class, 'pesanan'])->name('pesanan');
@@ -47,10 +51,10 @@ Route::middleware('UserMiddleware')->group(function () {
 
 Route::resource('user', UserController::class);
 Route::get('logout', [UserController::class, 'logout'])->name('logout');
-    
+
 
 // Route::middleware(['guest'])->group(function () {
-    Route::get('/', [UserController::class, 'dashboard'])->name('dashboard');
+    Route::get('/', [UserController::class, 'index'])->name('userdashboard');
     Route::get('register', [Usercontroller::class, 'register'])->name('register');
     Route::get('login', [Usercontroller::class, 'index'])->name('index');
     Route::post('authenticatelogin', [Usercontroller::class, 'authenticatelogin'])->name('authenticatelogin');
